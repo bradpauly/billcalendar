429cf30bcd57e99de6eae8a55e35916576c4af1f
Brad Pauly <hi@bradpauly.com>
2015-10-09 10:19:46 -0700

    Remove jQuery!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,28 +1,17 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+// https://github.com/remy/min.js
+var $=function(t,n,e){var i=Node.prototype,r=NodeList.prototype,o="forEach",u="trigger",c=[][o],s=t.createElement("i");return r[o]=c,n.on=i.on=function(t,n){return this.addEventListener(t,n,!1),this},r.on=function(t,n){return this[o](function(e){e.on(t,n)}),this},n[u]=i[u]=function(n,e){var i=t.createEvent("HTMLEvents");return i.initEvent(n,!0,!0),i.data=e||{},i.eventName=n,i.target=this,this.dispatchEvent(i),this},r[u]=function(t){return this[o](function(n){n[u](t)}),this},e=function(n){var e=t.querySelectorAll(n||"☺"),i=e.length;return 1==i?e[0]:e},e.on=i.on.bind(s),e[u]=i[u].bind(s),e}(document,this);
 
-$(function() {
+window.addEventListener('load', function(){
   $('table.due_day a').on('click', function(){
-    $('#bill_due_day').val($(this).html())
-    $('table.due_day td').removeClass('selected')
-    $(this).closest('td').addClass('selected')
-    return false;
+    $('#bill_due_day').value = this.innerHTML
+    $('table.due_day td').forEach(function(el, index){
+      el.className = 'not-selected'
+    })
+    this.parentElement.className = 'selected'
+    event.preventDefault();
   })
 
   $('#icalendar_url').on('click', function(){
-    $(this).select()
+    this.setSelectionRange(0, 100)
   })
-});
+}, false);

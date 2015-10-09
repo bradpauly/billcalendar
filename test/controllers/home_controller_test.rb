9bbb3263361567d008d03257bfe2c4fc0e3c1af7
Brad Pauly <hi@bradpauly.com>
2015-10-08 18:06:14 -0700

    Test homepage doesnt blow up.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "no errors!" do
+    get :index
+
+    assert_response :success
+    assert_template :index
+    assert assigns(:login_form)
+    assert assigns(:registration_form)
+  end
 end

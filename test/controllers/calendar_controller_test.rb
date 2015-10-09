c28f2b8f56dce2a46823f3601567a8bae1552201
Brad Pauly <hi@bradpauly.com>
2015-10-08 18:11:11 -0700

    Calendar controller test.

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CalendarControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "renders icalendar" do
+    jim = users(:jim)
+    get :index, token: jim.calendar_token, format: :ics
+
+    assert_response :success
+    assert assigns(:calendar)
+    assert assigns(:calendar).is_a?(BillIcalendar)
+  end
 end
